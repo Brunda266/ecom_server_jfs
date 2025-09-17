3ce341915bfacce2963cd2b6286825020446a0ed
Brundap266 <brundaparamesh4729@gmail.com>
2025-09-17 16:20:58 +0530

    Updated

--- src/main/java/com/mtd/ecom_server/controllers/ProductController.java
+++ src/main/java/com/mtd/ecom_server/controllers/ProductController.java
@@ -4,7 +4,6 @@ package com.mtd.ecom_server.controllers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +22,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin("*")
 @RestController 
 @RequestMapping("/products")
 public class ProductController {
